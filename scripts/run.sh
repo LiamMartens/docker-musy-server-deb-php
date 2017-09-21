1c9cc716aca0ee7d4c26e28b19bf4f0e65d29c3d
Liam Martens <hi@liammartens.com>
2017-09-21 14:16:26 +0200

    Fixed some permissions

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 chown -R www:www-data /var/www /etc/php/7.1
 chmod -R 755 /var/www /etc/php/7.1 && chmod -R g+w /var/www /etc/php/7.1 && chmod -R g+s /var/www /etc/php/7.1
+chmod 755 /var/www/stream/video/build/* && chmod g+w /var/www/stream/video/build/*
 
 export HOME=/home/www
 
@@ -21,18 +22,18 @@ echo $TIMEZONE > /etc/timezone
 
 # function to check php.ini config and bak
 checkPhpIni() {
-    if [[ `cat /etc/php/7.1/php.ini` =~ "^\s*$" ]]; then
-        mv /etc/php/7.1/php.ini.bak /etc/php/7.1/php.ini
+    if [[ `cat /etc/php/7.1/fpm/php.ini` =~ "^\s*$" ]]; then
+        mv /etc/php/7.1/fpm/php.ini.bak /etc/php/7.1/fpm/php.ini
     else
-        rm /etc/php/7.1/php.ini.bak
+        rm /etc/php/7.1/fpm/php.ini.bak
     fi
 }
 # function to check fpm www pool and bak
 checkPhpPool() {
-    if [[ `cat /etc/php/7.1/fpm.d/www.conf` =~ "^\s*$" ]]; then
-        mv /etc/php/7.1/fpm.d/www.conf.bak /etc/php/7.1/fpm.d/www.conf
+    if [[ `cat /etc/php/7.1/fpm/fpm.d/www.conf` =~ "^\s*$" ]]; then
+        mv /etc/php/7.1/fpm/fpm.d/www.conf.bak /etc/php/7.1/fpm/fpm.d/www.conf
     else
-        rm /etc/php/7.1/fpm.d/www.conf.bak
+        rm /etc/php/7.1/fpm/fpm.d/www.conf.bak
     fi
 }
 
@@ -51,20 +52,20 @@ for VAR in "${ENV_VARS[@]}"; do
         PHP_SETTING=$(echo $VAR_NAME | cut -d'_' -f 2-)
         PHP_SETTING=$(echo $PHP_SETTING | awk '{print tolower($0)}')
         PHP_SETTING=$(echo $PHP_SETTING | perl -pe "s/__/./")
-        perl -p -i.bak -e "s/^$PHP_SETTING\s*=\s*.*/$PHP_SETTING = $VAR_VALUE/gi" /etc/php/7.1/php.ini
+        perl -p -i.bak -e "s/^$PHP_SETTING\s*=\s*.*/$PHP_SETTING = $VAR_VALUE/gi" /etc/php/7.1/fpm/php.ini
         checkPhpIni
     else
         WWWVARS="$WWWVARS"$'\n'"env[$VAR_NAME]=$VAR_VALUE"
     fi
 done
-echo "$WWWVARS" > /etc/php/7.1/fpm.d/env.conf
+echo "$WWWVARS" > /etc/php/7.1/fpm/fpm.d/env.conf
 
 # set php listen port
-perl -p -i.bak -e "s/listen\s*=\s*(.+):.+/listen = $PHP_TCP:$PHP_PORT/gi" /etc/php/7.1/fpm.d/www.conf
+perl -p -i.bak -e "s/listen\s*=\s*(.+):.+/listen = $PHP_TCP:$PHP_PORT/gi" /etc/php/7.1/fpm/fpm.d/www.conf
 checkPhpPool
 
 # set timezone
-perl -p -i.bak -e "s/;*date.timezone\s*=.*/date.timezone = $TIMEZONE/gi" /etc/php/7.1/php.ini
+perl -p -i.bak -e "s/;*date.timezone\s*=.*/date.timezone = $TIMEZONE/gi" /etc/php/7.1/fpm/php.ini
 checkPhpIni
 
 # alsa
@@ -79,6 +80,7 @@ pcm.!default {
 EOF
 chown www:www ~/.asoundrc
 
+# start PHP FPM and take ownership
 echo "Starting PHP FPM on $PHP_PORT"
 /etc/init.d/php7.1-fpm start
 
