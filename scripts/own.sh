7a771fc3422ad09ba0653033b9817c458a415b0d
Liam Martens <hi@liammartens.com>
2017-11-10 12:56:07 +0100

    Modified own script

--- scripts/own.sh
+++ scripts/own.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chown -R www:www-data /var/www /etc/php/7.1
-chmod -R 755 /var/www /etc/php/7.1 && chmod -R g+w /var/www /etc/php/7.1 && chmod -R g+s /var/www /etc/php/7.1
+chown -R www:www-data /var/www /etc/php/7.1 /home/www
+chmod -R 755 /var/www /etc/php/7.1 /home/www && chmod -R g+w /var/www /etc/php/7.1 /home/www && chmod -R g+s /var/www /etc/php/7.1 /home/www
 chmod 755 /var/www/stream/video/build/* && chmod g+w /var/www/stream/video/build/*
