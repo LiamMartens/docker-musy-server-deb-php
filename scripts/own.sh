cb6d26dd4398efceb21220c095559f3a246b9059
Liam Martens <hi@liammartens.com>
2017-12-12 11:43:52 +0100

    SCripts

--- scripts/own.sh
+++ scripts/own.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chown -R www:www-data /var/www /etc/php/7.1 /home/www
 chmod -R 755 /var/www /etc/php/7.1 /home/www && chmod -R g+w /var/www /etc/php/7.1 /home/www && chmod -R g+s /var/www /etc/php/7.1 /home/www
-chmod 755 /var/www/stream/video/build/* && chmod g+w /var/www/stream/video/build/*
+if [[ -d /var/www/stream/video/build ]]; then
+    chmod -R 755 /var/www/stream/video/build && chmod -R g+ws /var/www/stream/video/build
+fi
