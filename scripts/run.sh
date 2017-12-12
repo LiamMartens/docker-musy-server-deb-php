84fe6cc6badb0af550262c9019ed6fc95098f016
Liam Martens <hi@liammartens.com>
2017-12-12 10:02:10 +0100

    Own on run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-chown -R www:www-data /var/www /etc/php/7.1
-chmod -R 755 /var/www /etc/php/7.1 && chmod -R g+w /var/www /etc/php/7.1 && chmod -R g+s /var/www /etc/php/7.1
-chmod 755 /var/www/stream/video/build/* && chmod g+w /var/www/stream/video/build/*
+
+# own files
+/home/www/own.sh
 
 export HOME=/home/www
 
