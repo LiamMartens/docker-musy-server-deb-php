4609d85c9fb87fe8b847dbded1da84c11f5ee534
Liam Martens <hi@liammartens.com>
2017-12-18 18:23:52 +0100

    Updated own script

--- scripts/own.sh
+++ scripts/own.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chown -R www:www-data /var/www /etc/php/7.1 /home/www
 chmod -R 755 /var/www /etc/php/7.1 /home/www && chmod -R g+w /var/www /etc/php/7.1 /home/www && chmod -R g+s /var/www /etc/php/7.1 /home/www
-if [[ -d /var/www/stream/video/build ]]; then
-    chmod -R 755 /var/www/stream/video/build && chmod -R g+w /var/www/stream/video/build
+if [[ -d /var/www/stream/video/musy-stream ]]; then
+    chmod -R 755 /var/www/stream/video/musy-stream && chmod -R g+w /var/www/stream/video/musy-stream
 fi
