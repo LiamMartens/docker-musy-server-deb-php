0d4c1d4cef5364ef18a65e716b823ae3a860588f
Liam Martens <hi@liammartens.com>
2017-12-19 18:24:43 +0100

    Fix user

--- scripts/own.sh
+++ scripts/own.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R www:www-data /var/www /etc/php/7.1 /home/www/.config /home/www
+chown -R :www-data /var/www /etc/php/7.1 /home/www/.config /home/www
 chmod -R 755 /var/www /etc/php/7.1 /home/www && chmod -R g+w /var/www /etc/php/7.1 /home/www && chmod -R g+s /var/www /etc/php/7.1 /home/www
 if [[ -d /var/www/stream/video/musy-stream ]]; then
     chmod -R 755 /var/www/stream/video/musy-stream
